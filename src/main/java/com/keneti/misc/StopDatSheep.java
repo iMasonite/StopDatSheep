8cadfeca7850624ffd70afaacea32e7980d49217
Michael Mason <mason1985@live.co.uk>
2017-01-03 15:57:10 +0000

    fix wool regrowth

--- src/main/java/com/keneti/misc/StopDatSheep.java
+++ src/main/java/com/keneti/misc/StopDatSheep.java
@@ -1,10 +1,13 @@
 package com.keneti.misc;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 /** @author Michael Mason */
 public class StopDatSheep extends JavaPlugin implements Listener {
@@ -18,7 +21,22 @@ public class StopDatSheep extends JavaPlugin implements Listener {
   public void onSheepEatGrass(EntityChangeBlockEvent event) {
     
     if(event.getEntity().getType() == EntityType.SHEEP) {
-      event.setCancelled(true);
+      
+      // WARNING: If you do this, sheep don't re-grow wool...
+      // event.setCancelled(true);
+      
+      // Do this instead...
+      final Location loc = event.getBlock().getLocation();
+      
+      if(event.getBlock().getType().equals(Material.GRASS) && event.getTo().equals(Material.DIRT)) {
+        
+        new BukkitRunnable() {
+          @Override
+          public void run() {
+            loc.getBlock().setType(Material.GRASS);
+          }
+        }.runTaskLater(this, 20L);
+      }
     }
   }
 }
